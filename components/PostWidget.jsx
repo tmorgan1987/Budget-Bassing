5c1e721e38598c8e0d5f5902541c393649e8817a
Troy Morgan <tmorgan1987@outlook.com>
2023-02-23 11:12:47 -0500

    cleaning up code

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -48,35 +48,3 @@ const PostWidget = ({categories, slug}) => {
 }
 
 export default PostWidget
-
-//   return (
-//     <div className="text-right relative overflow-hidden justify-around shadow-none pb-8 mb-8">
-//         <h3 className='text-gray-400 text-xl mb-8 font-semibold border-b pb-4'>
-//           {slug ? 'Related Reviews' : 'Recent Reviews'}
-//         </h3>
-//         {relatedPosts.map((post)=> (
-//           <div key={post.title} className='flex items-center w-full mb-4'>
-//             <div className='w-16 flex-none'>
-//               <img
-//               alt={post.title}
-//               height='120px'
-//               width='120px'
-//               className='align-middle rounded-full'
-//               src={post.featuredImage.url}
-//               />
-//             </div>
-//             <div className='flex-grow ml-4 text-white'>
-//             <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>
-//                 {post.title}
-//               </Link>
-//               <p className='text-white font-xs'>
-//                 {moment(post.createdAt).format('MMM DD, YYYY')}
-//               </p>
-//             </div>
-//           </div>
-//         ))}
-//     </div>
-//   )
-// }
-
-// export default PostWidget
